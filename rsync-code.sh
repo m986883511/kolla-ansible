29800945eddf9034871eeaf58800217f3c54f083
wang.chao <wang.chao@astute-tec.com>
2023-04-26 14:37:52 +0800

    add kyline

--- rsync-code.sh
+++ rsync-code.sh
@@ -8,14 +8,18 @@ SCRIPT_DIR="$(basename $(cd "$(dirname "$0")" && pwd -P))"
 
 echo "当前脚本文件所在的目录：$SCRIPT_DIR"
 
-# 判断是否只传了1个参数，并且传入的参数是否是ip
-if [ $# -eq 1 ] && [[ $1 =~ ^[0-9]+\.[0-9]+\.[0-9]+\.[0-9]+$ ]]; then
-    remote_ip=$1
-else
-    echo "ERROR: 需要传入一个ip地址"
+# 判断是否只传了2个参数，并且判断传入的第一个参数是ip，第二个参数是root密码
+if [ $# -ne 2 ]; then
+    echo "ERROR: Usage: ip root_password"
     exit 1
+else
+    remote_ip=$1
+    if [[ ! $remote_ip =~ ^[0-9]+\.[0-9]+\.[0-9]+\.[0-9]+$ ]]; then
+        echo "ERROR: Usage: param1 should be ip"
+        exit 1
+    fi
+    root_password=$2
 fi
 
-
-sshpass -p donotuseroot! ssh -o 'StrictHostKeyChecking no' root@$remote_ip "mkdir -p /root/rsync/$SCRIPT_DIR"
-rsync -avz -e "sshpass -p donotuseroot! ssh -o 'StrictHostKeyChecking no'" --exclude "*.git" . root@$remote_ip:/root/rsync/$SCRIPT_DIR
+sshpass -p $root_password ssh -o 'StrictHostKeyChecking no' root@$remote_ip "mkdir -p /root/rsync/$SCRIPT_DIR"
+rsync -avz -e "sshpass -p $root_password ssh -o 'StrictHostKeyChecking no'" --exclude "*.git" . root@$remote_ip:/root/rsync/$SCRIPT_DIR
